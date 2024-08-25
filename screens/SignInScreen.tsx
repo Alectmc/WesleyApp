66fad29316057d539fc7595ebd957c4b17524e31
Alectmc <alectmc@gmail.com>
2024-08-24 21:31:32 -0500

    Added loading animation and rewrote handleSignIn() into try/catch syntax

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, Alert, StyleSheet } from 'react-native';
+import { View, Text, Button, Alert, ActivityIndicator, StyleSheet } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import moment from 'moment';
 import 'moment-timezone';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+import FooterText from '../footer/FooterText';
 
 const SignInScreen = () => {
     const [selectedEvent, setSelectedEvent] = useState('tuesday_worship');
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const events = [
         { label: 'Tuesday Worship', value: 'tuesday_worship', start: '18:30', end: '20:00', day: 2 },
         { label: 'Wednesday Lunch', value: 'wednesday_lunch', start: '10:45', end: '13:00', day: 3 },
-        { label: 'Thursday Dinner & Bible Study', value: 'thurday_dinner', start: '12:30', end: '20:00', day: 5 }
+        { label: 'Thursday Dinner & Bible Study', value: 'thurday_dinner', start: '17:30', end: '20:00', day: 4 }
     ];
 
     useEffect(() => {
@@ -32,31 +34,68 @@ const SignInScreen = () => {
         loadUserInfo();
     }, []);
 
-    const handleSignIn = () => {
+    const handleSignIn = async () => {
         const event = events.find(e => e.value === selectedEvent);
 
         const now = moment().tz('America/Chicago');
         const startTime = moment(now).day(event.day).hour(event.start.split(':')[0]).minute(event.start.split(':')[1]);
         const endTime = moment(now).day(event.day).hour(event.end.split(':')[0]).minute(event.end.split(':')[1]);
 
-        if (now.isBetween(startTime, endTime)) {
+        if(!now.isBetween(startTime, endTime)) {
+            const formattedTime = startTime.format('h:mm A');
+            Alert.alert("Error", `${event.label} sign in is not open right now! You may sign in starting at ${formattedTime} on ${event.label.split(' ')[0]}.`);
+            return;
+        }
+
+        setLoading(true)
+        try {
+            const response = await axios.post('http://172.16.100.30:4000/signin', {
+                firstName,
+                lastName,
+                email
+            }, {
+                timeout: 10000
+            });
+            Alert.alert("Success", `You have signed in as ${firstName} ${lastName} for ${event.label}!`);
+        } catch (e) {
+            console.log(e)
+            const error = e as AxiosError;
+            if (error.message === 'Network Error'){
+                Alert.alert('Network Error', 'Signing in took too long to complete. Please try again later.');
+            } else{
+                Alert.alert('Error', 'Could not sign in due to an unknown error. Please try again later.');
+            }
+        }
+        setLoading(false);
+
+        /*if (now.isBetween(startTime, endTime)) {
+            setLoading(true);
             //POST request to backend server (Will be localhost for now during dev testing!)
             axios.post('http://192.168.1.154:4000/signin', {
                 firstName,
                 lastName,
                 email,
+            }, {
+                timeout: 10000
             })
             .then(response => {
                 Alert.alert("Success", `You have signed in as ${firstName} ${lastName} for ${event.label}!`);
+                setLoading(false);
             })
-            .catch(error => {
-                Alert.alert('Error', 'Could not sign in. Please try again later.')
-                console.log(error)
+            .catch(e => {
+                console.log(e)
+                const error = e as AxiosError;
+                if (error.message === 'Network Error'){
+                    Alert.alert('Network Error', 'Signing in took too long to complete. Please try again later.')
+                } else{
+                    Alert.alert('Error', 'Could not sign in. Please try again later.')
+                }
+                setLoading(false);
             });
         } else {
             const formattedTime = startTime.format('h:mm A');
             Alert.alert("Error", `${event.label} sign in is not open right now! You may sign in starting at ${formattedTime} on ${event.label.split(' ')[0]}.`);
-        }
+        }*/
     };
 
     return (
@@ -71,7 +110,15 @@ const SignInScreen = () => {
                     <Picker.Item key={event.value} label={event.label} value = {event.value} />
                 ))}
             </Picker>
-            <Button title="Sign In" onPress={handleSignIn} />
+            {loading ? (
+                <ActivityIndicator size='large' color='blue' />
+            ) : (
+                <>
+                    <Button title="Sign In" onPress={handleSignIn} />
+                </>
+            )}
+
+            <FooterText />
         </View>
     );
 };
