722e0dec1d547f3219254a301cecf61f3a225a12
Alectmc <alectmc@gmail.com>
2024-08-24 21:34:52 -0500

    Footer text for bottom left of UI showing user this is a development build of the app

---
+++ footer/FooterText.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+
+const FooterText = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.textStyle}>DEVELOPMENT BUILD: v0.1b</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        left: 10,
+        bottom: 10
+    },
+    textStyle: {
+        fontSize: 12,
+        color: '#808080'
+    }
+});
+
+export default FooterText;
