226d0d3f0bfbd347cd626698f39c57f5d9b7d8f2
Alec Creasy <alectmc@gmail.com>
2024-11-25 16:20:16 -0600

    Changed whats new for 0.4.1

--- screens/WhatsNewScreen.tsx
+++ screens/WhatsNewScreen.tsx
@@ -1,55 +1,16 @@
 import React, { useState } from 'react';
-import { View, Text, Button, Image, Alert, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import FooterText from '../footer/FooterText';
 import axios, { AxiosError } from 'axios'; //Using 0.24.0
 
 const WhatsNewScreen = () => {
-    //const [updateMessage, setUpdateMessage] = useState('');
-    /*const [loading, setLoading] = useState(false);
-
-    const checkAppVersion = async () => {
-        setLoading(true);
-
-        try {
-            const response = await axios.post('http://172.16.100.30:5000/check-version', {
-                version: 'v0.1b'
-            }, {
-                timeout: 5000
-            });
-            const data = response.data;
-            if (data.latest) {
-                Alert.alert("You're up-to-date!", 'The Wesley App is up-to-date and running on the latest version.');
-                //setUpdateMessage('Your app is up-to-date!')
-            } else {
-                Alert.alert("Out-Of-Date!", `The Wesley App is out-of-date! To continue using the sign-in and prayer request features, please update to the newest version: ${data.latestVersion}`);
-                //setUpdateMessage(`OUT OF DATE. AN UPDATE IS AVAILABLE: ${data.latestVersion}`);
-            }
-        } catch (e) {
-            console.warn(e);
-
-            const error = e as AxiosError;
-
-            if (error.message === 'Network Error') { //ECONNABORTED is the error code in axios for a timed out request.
-                Alert.alert('Network Error', 'Checking for updates took too long. Please try again later.')
-            } else {
-                Alert.alert('Error', 'An error has occured. Please try again later.')
-                //setUpdateMessage('An error occured during version check. Please try again later.')
-            }
-        }
-        setLoading(false);
-    };*/
-
     return(
         <View style={styles.container}>
             <Image source={require('../assets/Blobs.png')} style={styles.imageStyle} />
-            <Text style={styles.mainText}>What's New in Beta 0.3?</Text>
-            <Text style={styles.list}>-The Wesley App is now in OPEN BETA!!!!!</Text>
-            <Text style={styles.list}>-Calendar and Link Tree are now available!</Text>
-            <Text style={styles.list}>-Prayer Requests are now being tested!</Text>
-            <Text style={styles.list}>-Notifications are on the to-do list!</Text>
-            <Text style={styles.list}>-Sign-Ins Are Coming Soon!</Text>
-            <Text style={styles.list}>-What's New section added to view what's new in each update!</Text>
-
+                <Text style={styles.mainText}>What's New in Beta 0.4.1?</Text>
+                <Text style={styles.list}>-Fixed a bug where the keyboard would cover text fields.</Text>
+                <Text style={styles.list}>-Cleaned up keyboard layout.</Text>
+                <Text style={styles.list}>-Fixed a bug where the user could not scroll on the setup screen.</Text>
             <FooterText />
         </View>
     )
@@ -80,17 +41,8 @@ const styles = StyleSheet.create({
     imageStyle: {
         position: 'absolute',
         width: '112%',
-        height: '112%'
+        height: '112%',
+        zIndex: -1
     }
 })
 export default WhatsNewScreen
-
-/*
-{loading ? (
-                <ActivityIndicator size='large' color='blue' />
-            ) : (
-                <>
-                    <Button title="Check For Update" onPress={checkAppVersion} />
-                </>
-            )}
-*/
