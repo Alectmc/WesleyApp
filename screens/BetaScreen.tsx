305964efc9baab2c17ff46c6f4d827b4dd6bfc57
Alec Creasy <alectmc@gmail.com>
2024-12-30 21:14:53 -0500

    Added comments and slightly changed the wording for the Beta disclaimer

--- screens/BetaScreen.tsx
+++ screens/BetaScreen.tsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { View, Button, Text, Alert, StyleSheet } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+//Screen to ensure the user is aware that this is a beta build of the app.
 const BetaScreen = ({ navigation }) => {
+    //The following code sets the async storage's item 'agree_terms' to true,
+    //indicating the user has agreed to the acknowledgement. Assuming no errors,
+    //the user will be redirected to the home page and never seen this screen again.
     const agree = 'true';
 
     const agreeToTerms = async () => {
@@ -15,12 +19,13 @@ const BetaScreen = ({ navigation }) => {
         }
     }
 
+    //View of the screen for the end user.
     return(
         <View style={styles.container}>
             <Text style={styles.betaText}>BETA DISCLAIMER</Text>
             <Text style={styles.descText}>This is a beta build of the Wesley App.</Text>
             <Text style={styles.descText}>Some things may not work properly, and this build is <Text style={styles.underline}>NOT</Text> reflective of the final version.</Text>
-            <Text style={styles.descText}>To get started testing the app, simply tap 'I Understand' below!</Text>
+            <Text style={styles.descText}>To get started using the app, simply tap 'I Understand' below!</Text>
             <View style={styles.buttonStyle}>
                 <Button title="I Understand" onPress={agreeToTerms} />
             </View>
@@ -28,6 +33,7 @@ const BetaScreen = ({ navigation }) => {
     )
 }
 
+//Stylesheets for screen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
